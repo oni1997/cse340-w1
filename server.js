8a32894cfe4f520ede915f52ce2ab0e13bf384d9
oni1997 <dzidzaimaenza@gmail.com>
2025-05-13 00:36:00 +0200

    Initial commit: Set up Express application with EJS templating

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+
+const routes = require('./routes/index');
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
