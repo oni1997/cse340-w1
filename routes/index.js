f0a2046c1c3b32796c18ec49fe0ad0c110307d37
oni1997 <dzidzaimaenza@gmail.com>
2025-05-13 00:44:05 +0200

    minor updates thought maybe it should be more me focused

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Onesmus Auto Shop | Home',
+    description: 'Welcome to Onesmus Auto Shop, your premier dealership for luxury and classic cars.'
+  });
 });
 
 module.exports = router;
